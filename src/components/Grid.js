b6855e5ae8a6c2590011827c6990f8b2b45df04a
Everton Pereira <everton.mwp@gmail.com>
2019-02-19 22:37:54 -0300

    fixing the grid/css

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,7 @@ class Grid extends Component {
   render() {
     return (
       <Wrapper>
+        <h1>Periodic Table</h1>
         <GridLayout>
           <Element>1</Element>
           <Element>2</Element>
@@ -63,14 +64,23 @@ const Element = styled.div`
 `;
 
 const GridLayout = styled.div`
+  /* width: 90%; */
   box-sizing: border-box;
   display: grid;
   grid-template-columns: repeat(17, auto) 1fr;
+  width: 90%;
+
+  /* background: yellow; */
 `;
 
 const Wrapper = styled.div`
+  /* background: red; */
   width: 90%;
-  margin: 0;
+  margin: 0 5%;
+
+  h1 {
+    text-align: center;
+  }
 `;
 
 export default Grid;
