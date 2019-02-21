8354b1b83af88ebf84608c53e8241f414a52b334
Everton Pereira <everton.mwp@gmail.com>
2019-02-20 22:03:12 -0300

    bg color elements

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -8,16 +8,204 @@ const Elements = () => {
       {data.elements.map(el => {
         let xpos = el.xpos;
         let ypos = el.ypos;
-        console.log(xpos, ypos);
-        return (
-          <Element
-            key={el.name}
-            style={{ gridRowStart: ypos, gridColumnStart: xpos }}
-          >
-            <div className="element_number">{el.number}</div>
-            <div className="element_symbol">{el.symbol}</div>
-          </Element>
-        );
+        if (el.category === 'alkaline earth metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'yellow'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'noble gas') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'grey'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'alkali metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'red'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'transition metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'white'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'diatomic nonmetal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'blue'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'polyatomic nonmetal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'green'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'post-transition metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'purple'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'metalloid') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'orange'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'lanthanide') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'salmon'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'actinide') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'purple'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'unknown, probably transition metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'white'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'unknown, probably post-transition metal') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'white'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'unknown, probably metalloid') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'red'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        } else if (el.category === 'unknown, predicted to be noble gas') {
+          return (
+            <Element
+              key={el.name}
+              style={{
+                gridRowStart: ypos,
+                gridColumnStart: xpos,
+                background: 'black'
+              }}
+            >
+              <div className="element_number">{el.number}</div>
+              <div className="element_symbol">{el.symbol}</div>
+            </Element>
+          );
+        }
+        return null;
       })}
     </React.Fragment>
   );
