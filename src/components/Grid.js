d60d5cc4ae7dd61405118ff9433912205321e5fa
Everton Pereira <everton.mwp@gmail.com>
2019-03-23 22:32:58 -0300

    updated contact link

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import Elements from './Elements';
+import GitHub from '../imgs/github.svg';
 
 const Grid = props => {
   return (
     <Wrapper>
       <h1>Periodic Table</h1>
-      <div>
-        <HeaderInfo>
-          <p>
-            Made by{' '}
-            <a
-              href="https://emwp.me/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Everton Pereira
-            </a>
-          </p>
-          <a
-            href="https://github.com/emwp/periodic-table"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github"> Source Code</i>
-          </a>
-        </HeaderInfo>
-      </div>
       <GridLayout>
         <Elements />
       </GridLayout>
       <div>
         <h1>{props.name}</h1>
+      </div>
+      <div>
+        <HeaderInfo>
+          <p>Contact Me</p>
+          <a
+            href="https://github.com/emwp/periodic-table"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GitHub} alt="GitHub" />
+          </a>
+        </HeaderInfo>
       </div>
     </Wrapper>
   );
@@ -56,10 +48,20 @@ const Wrapper = styled.div`
 
 const HeaderInfo = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   font-size: 1.7vw;
+
+  img {
+    width: 3vw;
+    transition: 0.3s ease-out;
+  }
+
+  img:hover,
+  img:active {
+    filter: invert(0.3);
+  }
 `;
 
 export default Grid;
